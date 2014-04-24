d64b7b1c03a9af316a400358d900e30c23526d4a
Vincent Gu <g@v-io.co>
2014-04-24 20:03:47 +0800

    move 'flume-ng-syncdir-sink' out of 'flume-ng-core'
    Conflicts:
    	flume-ng-dist/pom.xml
    	flume-ng-sinks/pom.xml
    
    Conflicts:
    	flume-ng-dist/pom.xml
    	flume-ng-sinks/pom.xml
    	pom.xml

---
+++ flume-ng-sinks/flume-ng-syncdir-sink/src/main/java/org/apache/flume/sink/syncdir/SyncDirSinkConfigurationConstants.java
@@ -0,0 +1,18 @@
+package org.apache.flume.sink.syncdir;
+
+public class SyncDirSinkConfigurationConstants {
+  /** Directory to sync. */
+  public static final String SYNC_DIRECTORY = "directory";
+
+  /** serializer selection and config */
+  public static final String SERIALIZER = "serializer";
+  public static final String DEFAULT_SERIALIZER = "TEXT";
+  public static final String SERIALIZER_PREFIX = SERIALIZER + ".";
+
+  /** Header in which to put relative filename. */
+  public static final String FILENAME_HEADER_KEY = "path";
+
+  /** What size to batch with before sending to ChannelProcessor. */
+  public static final String BATCH_SIZE = "sink.batchSize";
+  public static final int DEFAULT_BATCH_SIZE = 100;
+}
